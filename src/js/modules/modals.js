7d22a1d7fff395aaa0302cc2ab7897aabfd76601
Artem <lunin.mail@gmail.com>
2021-12-25 10:20:46 +0200

    Animation.Scroll fixed. Prod

--- src/js/modules/modals.js
+++ src/js/modules/modals.js
@@ -3,7 +3,8 @@ const modals = () => {
 		const trigger = document.querySelectorAll(triggerSelector), 
 			modal = document.querySelector(modalSelector), 
 			close = document.querySelector(closeSelector),
-			windows = document.querySelectorAll('[data-modal]');
+			windows = document.querySelectorAll('[data-modal]'),
+			scroll = calcScroll();
 
 		trigger.forEach(item => {
 			item.addEventListener('click', (e) => {
@@ -18,6 +19,7 @@ const modals = () => {
 				modal.style.display = "block";
 				// document.body.classList.add('modal-open')
 				document.body.style.overflow = "hidden";
+				document.body.style.marginRight = `${scroll}px`;
 			});
 		});
 
@@ -29,6 +31,7 @@ const modals = () => {
 			modal.style.display = "none";
 			// document.body.classList.remove('modal-open')
 			document.body.style.overflow = "";
+			document.body.style.marginRight = `0px`;
 		});
 
 		modal.addEventListener('click', (e) => {
@@ -40,6 +43,7 @@ const modals = () => {
 				modal.style.display = "none";
 				// document.body.classList.remove('modal-open')
 				document.body.style.overflow = "";
+				document.body.style.marginRight = `0px`;
 			}
 		});
 	}
@@ -51,6 +55,22 @@ const modals = () => {
 		}, time);
 	}
 
+	function calcScroll() {
+		let div = document.createElement('div');
+
+		div.style.width = '50px';
+		div.style.height = '50px';
+		div.style.overflowY = 'scroll';
+		div.style.visibility = 'hidden'
+
+		document.body.append(div);
+
+		let scrollWidth = div.offsetWidth - div.clientWidth;
+		div.remove();
+
+		return scrollWidth;
+	}
+	
 	bindModal('.popup_engineer_btn', '.popup_engineer', '.popup_engineer .popup_close');
 	bindModal('.phone_link', '.popup', '.popup .popup_close');
 	bindModal('.popup_calc_btn', '.popup_calc', '.popup_calc_close');
